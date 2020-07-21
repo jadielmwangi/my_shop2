9a05e9b50f7ab10ebf8319aa9a04a6f010923cad
jadielmwangi <jedielmwangi@gmail.com>
2020-07-21 12:11:49 +0300

    adds celery

--- myshop/settings.py
+++ myshop/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cart.apps.CartConfig',
+    'orders.apps.OrdersConfig',
 ]
 
 MIDDLEWARE = [
@@ -63,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.context_processors.cart',
             ],
         },
     },
@@ -124,5 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+CART_SESSION_ID = 'cart'
--- shop/views.py
+++ shop/views.py
@@ -3,6 +3,7 @@
 # Create your views here.
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
+from cart.forms import CartAddProductForm
 
 
 def product_list(request, category_slug=None):
@@ -19,12 +20,26 @@ def product_list(request, category_slug=None):
             'products': products})
 
 
-def product_detail(request, id, slug):
-    product = get_object_or_404(Product,
-                            id=id,
-                            slug=slug,
-                            available=True)
-    return render(request,
-                'shop/product/detail.html',
-                  {'product': product})          
+# def product_detail(request, id, slug):
+#     product = get_object_or_404(Product,
+#                             id=id,
+#                             slug=slug,
+#                             available=True)
+#     return render(request,
+#                 'shop/product/detail.html',
+#                   {'product': product})          
                 
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product, id=id,
+                        slug=slug,
+                        available=True)
+    cart_product_form = CartAddProductForm()
+    return render(request,
+                    'shop/product/detail.html',
+                    {'product': product,
+                    'cart_product_form': cart_product_form})
+
+
+
+
+    
